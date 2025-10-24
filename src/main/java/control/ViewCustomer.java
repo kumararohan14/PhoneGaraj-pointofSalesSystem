e7e49d5282bf2058eb97264a3ccc1a43565c5014
Pradeep Rohan <kumararoha14@gmail.com>
2025-10-24 19:25:15 +0530

    setup project with layered architecture skelton and DB connection

--- src/main/java/control/ViewCustomer.java
+++ src/main/java/control/ViewCustomer.java
@@ -1,4 +1,66 @@
 package control;
 
+import Model.Customer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class ViewCustomer {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TableView<Customer> tblCustomer;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    ObservableList<Customer> customerObservableList = FXCollections.observableArrayList();
+
+    public void loadTable(){
+        colID.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+
+        tblCustomer.setItems(customerObservableList);
+    }
+
+    public void btnAddCustomerOnAction(ActionEvent actionEvent) {
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        Double salary = Double.parseDouble(txtSalary.getText());
+
+        Customer customer = new Customer(id, name, address, salary);
+
+        customerObservableList.add(customer);
+        loadTable();
+
+    }
 }
